524ecce5a220370ec219b2f7527151bfb55c2d32
Clifton Ferguson <clifton.ferguson7@gmail.com>
2019-06-22 15:35:59 -0500

    Controller spec auth

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -26,13 +26,39 @@ RSpec.describe WikisController, type: :controller do
    # end
 
    # describe "GET new" do
-   #   it "returns http success" do
-   #     get :new
-   #     expect(response).to have_http_status(:success)
-   #   end
-   # end
+   #    it "returns http success" do
+   #      get :new
+   #      expect(response).to have_http_status(:success)
+   #    end
 
-   # describe "GET edit" do
+   #    it "renders the #new view" do
+   #      get :new
+   #      expect(response).to render_template :new
+   #    end
+
+   #    it "instantiates @wiki" do
+   #      get :new
+   #      expect(assigns(:wiki)).not_to be_nil
+   #    end
+   #  end
+
+   #  describe "WIKI create" do
+
+   #    it "increases the number of Wiki by 1" do
+   #      expect{ wiki :create, params: { wiki: { title: RandomData.random_sentence, body: RandomData.random_paragraph } } }.to change(Post,:count).by(1)
+   #    end
+
+   #    it "assigns the new wiki to @wiki" do
+   #      wiki :create, params: { wiki: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
+   #      expect(assigns(:wiki)).to eq Wiki.last
+   #    end
+      
+   #    it "redirects to the new wiki" do
+   #      wiki :create, params: { wiki: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
+   #      expect(response).to redirect_to Wiki.last
+   #    end
+   #  end
+   # # describe "GET edit" do
    #   it "returns http success" do
    #     get :edit
    #     expect(response).to have_http_status(:success)
