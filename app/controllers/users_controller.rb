dbebc076630ab2b9f6597d1e5e8aa1b8daf442c9
Clifton Ferguson <clifton.ferguson7@gmail.com>
2019-07-03 20:19:05 -0500

    role change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,14 @@ class UsersController < ApplicationController
 	def change_role
     end
    
+    def update_role
+    	email = params[:email].strip!
+        role = params[:role]
+
+        if !['admin', 'premium', 'standard'].include? role
+            flash[:notice] = "Invalid role set for user"
+            return redirect_to '/users/change-role'
+        end
+    end
 
 end
