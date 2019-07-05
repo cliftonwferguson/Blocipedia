253a3db3893daefb15306f722ca6e1c5996bebf8
Clifton Ferguson <clifton.ferguson7@gmail.com>
2019-07-04 19:10:10 -0500

    User update functioning

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,40 @@ class UsersController < ApplicationController
     end
 	
 	def change_role
+		@users = User.all 
     end
    
     def update_role
-    	email = params[:email].strip!
+    	email = params[:email].strip
         role = params[:role]
+
+        user = User.find_by_email(email)
+        print 'EMAIL', email, 'USER', user
 
         if !['admin', 'premium', 'standard'].include? role
             flash[:notice] = "Invalid role set for user"
             return redirect_to '/users/change-role'
         end
+        
+        user = User.find_by_email(email)
+         if user == nil
+         	flash[:notice] = "User is not found."
+         	return redirect_to '/users/change-role'
+
+         else
+            user.role = role
+            user.save
+          end
+
+         if user.save
+    	  flash[:notice] = "User was updated."
+    	  
+        else
+    	flash.now[:alert] = "There was an error updating the user. Please try again."
+    	render :edit
+    end
+
+
     end
 
 end
