0b5b859e4f49c7f8cf7cd75155f4f2508446e28e
Clifton Ferguson <clifton.ferguson7@gmail.com>
2019-07-07 16:08:05 -0500

    Functioning Downgrade

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -9,14 +9,10 @@ def new
  end
 
  def destroy
-  current_user.downgrade("role", "standard") 
-
+  current_user.update(role: "standard") 
+  current_user.role == :standard
   flash[:notice] = "You have now been downgraded to a standard membership."
   redirect_to user_path(current_user)
-  
-  after_action do
-    current_user.role == :standard
-  end
   
  end
 
